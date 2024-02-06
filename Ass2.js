9f8ff3288867048bca1bf733efe4cf94956ac846
Afnan031 <afnanmemon28@gmail.com>
2024-02-06 16:05:08 +0500

    first commit

---
+++ Ass2.js
@@ -0,0 +1,67 @@
+
+// Question-1   
+           // Answer //
+
+// var num = 10
+// console.log('The Current value of a is: ', num)
+
+// console.log('The value of ++a is: ', ++num)
+// console.log('Now, the value of ++a is: ', ++num)
+
+// console.log('The value of ++a is: ', num)
+// console.log('Now, the value of ++a is: ', ++num)
+
+// console.log('The value of ++a is: ', num)
+// console.log('Now, the value of --a is: ', --num)
+
+// console.log('The value of ++a is: ', num)
+// console.log('Now, the value of --a is: ', --num)
+
+
+
+// Question-2   
+           // Answer //
+
+// var a = 2, b = 1
+// var results =--a - --b + ++b;
+// console.log('a =', a)
+// console.log('b =', b)
+// console.log('results =',results )
+
+// Question-3   
+           // Answer //
+
+// var program = prompt('Please Enter Your Name')
+// alert('Welcome to Our Website')
+
+
+// Question-4   
+           // Answer //
+
+// var defaults = 5
+// var number = 25
+// var secondNumber = prompt('Enter the number you want to multiply with 25')
+// var total = number*secondNumber
+// if(total= number*secondNumber){
+//     console.log(total)
+// }else{
+//     console.log(number*defaults)
+// }
+
+
+// Question-5   
+           // Answer //
+           
+// var subjects = subject1, subject2, subject3
+// var outofMarks1 = 100, outofMarks2 = 100, outofMarks3 = 100
+// var outofMarks = outofMarks1+outofMarks2+outofMarks3
+// var subject1 = prompt('Enter Your 1st Subject'), subject2 = prompt('Enter Your 2nd Subject'), subject3 = prompt('Enter Your 3rd Subject') 
+// var marks1 =+ prompt('Enter Your Obtained Marks of 1st Subject Out Of 100'), marks2 =+ prompt('Enter Your Obtained Marks of 2nd Subject Out Of 100'), marks3 =+ prompt('Enter Your Obtained Marks of 3rd Subject Out Of 100')
+// var totalMarks =+ marks1+marks2+marks3
+// var percentage =+ totalMarks*100/outofMarks
+// console.log(subject1,': ',  marks1)
+// console.log(subject2,': ',  marks2)
+// console.log(subject3,': ',  marks3)
+// console.log('Total Marks: ', outofMarks)
+// console.log('Obtained Marks: ', totalMarks)
+// console.log('Percentage: ', percentage) 
